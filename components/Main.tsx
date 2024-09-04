871d6df8893a9853adb03ee73f0ddf9bba796e8f
Sunwoo Jung <49130227+InHeritas@users.noreply.github.com>
2024-09-04 19:00:40 +0900

    Fix download button

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,12 +1,53 @@
 import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client'; // createRoot는 이곳에서 가져옵니다.
 import Lottie from "react-lottie-player";
 import loadingCircle from "../public/lottie/loading-circle.json";
 import { Button } from "@/components/ui/button";
+import Link from 'next/link'
 import { MagicWandIcon, LetterCaseCapitalizeIcon, DownloadIcon } from '@radix-ui/react-icons';
 import useTranslation from 'next-translate/useTranslation';
 
 const Main: React.FC = () => {
     const { t } = useTranslation('common');
+
+    useEffect(() => {
+        const observer = new MutationObserver((mutationsList) => {
+            mutationsList.forEach((mutation) => {
+                if (mutation.type === 'childList') {
+                    mutation.addedNodes.forEach((node) => {
+                        if (node.nodeName === 'A' && node.parentElement?.id === 'convert-result-container') {
+                            const aElement = node as HTMLAnchorElement;
+                            const buttonWrapper = document.createElement('div');
+
+                            const root = ReactDOM.createRoot(buttonWrapper);
+                            root.render(
+                                <Button asChild className="bg-blue-500 text-white hover:bg-blue-600">
+                                    <Link href={aElement.href} download={aElement.download}>
+                                        <DownloadIcon className="mr-3 h-4 w-4" />
+                                        {aElement.textContent}
+                                    </Link>
+                                </Button>
+                            );
+
+                            aElement.replaceWith(buttonWrapper);
+                        }
+                    });
+                }
+            });
+        });
+
+        const targetNode = document.getElementById('convert-result-container');
+        if (targetNode) {
+            observer.observe(targetNode, {
+                childList: true,
+                subtree: true,
+            });
+        }
+
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
 
     return (
         <main className="container" >
